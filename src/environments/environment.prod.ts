da91a5bdfd320821447fcdd0e09c11ff66f1f79a
Marjory.BRAVO@fittingbox.com <marjory.bravo@fittingbox.com>
2025-11-19 13:57:58 +0100

    fix(prod): add correct serverUrl prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // serverUrl: 'http://localhost:3000'
-  serverUrl: 'http://10.0.2.2:3000'
-
+    serverUrl: 'https://d29qqvaob7rvyi.cloudfront.net'
 };
